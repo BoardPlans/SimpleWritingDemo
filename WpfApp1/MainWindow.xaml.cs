7a460e429fe29fd83e109adca96f9355fc96a15f
冬 余 <1016724958@qq.com>
2021-03-07 01:25:09 +0800

    添加擦除

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -14,42 +14,75 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfApp1
+namespace InkWritingByCanvasDemo
 {
     /// <summary>
-    /// MainWindow.xaml 的交互逻辑
+    /// InkCanvas实现书写的最简单DEMO
     /// </summary>
     public partial class MainWindow : Window
     {
+        private InkCanvasEditingMode _editingMode;
+
         public MainWindow()
         {
             InitializeComponent();
             BoardInkCanvas.EditingMode = InkCanvasEditingMode.Ink;
-            BoardInkCanvas.DefaultDrawingAttributes=new DrawingAttributes()
+            BoardInkCanvas.DefaultDrawingAttributes = new DrawingAttributes()
             {
                 Color = Colors.Red,
                 FitToCurve = true,
                 Height = 6,
                 Width = 6,
-                IgnorePressure = true,
+                IgnorePressure = false,
                 IsHighlighter = true,
             };
+            BoardInkCanvas.EraserShape = new RectangleStylusShape(100, 200);
         }
 
         private void SelectButton_OnChecked(object sender, RoutedEventArgs e)
         {
-            BoardInkCanvas.EditingMode = InkCanvasEditingMode.Select;
+            BoardInkCanvas.EditingMode = _editingMode = InkCanvasEditingMode.Select;
         }
 
         private void PenButton_OnChecked(object sender, RoutedEventArgs e)
         {
-            BoardInkCanvas.EditingMode = InkCanvasEditingMode.InkAndGesture;
+            BoardInkCanvas.EditingMode = _editingMode = InkCanvasEditingMode.InkAndGesture;
         }
 
         private void EraserButton_OnChecked(object sender, RoutedEventArgs e)
         {
-            BoardInkCanvas.EditingMode = InkCanvasEditingMode.EraseByPoint;
-            BoardInkCanvas.EraserShape=new RectangleStylusShape(100,200);
+            _editingMode = InkCanvasEditingMode.EraseByPoint;
+        }
+
+        private void BoardInkCanvas_OnPreviewStylusDown(object sender, StylusDownEventArgs e)
+        {
+            if (_editingMode == InkCanvasEditingMode.EraseByPoint)
+            {
+                BoardInkCanvas.EditingMode = _editingMode;
+            }
+        }
+
+        private void BoardInkCanvas_OnPreviewStylusUp(object sender, StylusEventArgs e)
+        {
+            if (_editingMode == InkCanvasEditingMode.EraseByPoint)
+            {
+                BoardInkCanvas.EditingMode = InkCanvasEditingMode.None;
+            }
+        }
+
+        private void BoardInkCanvas_OnPreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (_editingMode == InkCanvasEditingMode.EraseByPoint)
+            {
+                BoardInkCanvas.EditingMode = _editingMode;
+            }
+        }
+        private void BoardInkCanvas_OnPreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (_editingMode == InkCanvasEditingMode.EraseByPoint)
+            {
+                BoardInkCanvas.EditingMode = InkCanvasEditingMode.None;
+            }
         }
     }
 }
